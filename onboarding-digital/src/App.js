f32a18c9dc1a730ba6d68ac705a19cccaefe90de
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-01-29 17:35:08 +0100

    exercici 2

--- onboarding-digital/src/App.js
+++ onboarding-digital/src/App.js
@@ -11,7 +11,19 @@ function App() {
       tit: 'Dedica muchas horas',
       desc: 'Un minimo de 30 horas a la semana. Si no tienes suficiente, tendras que dedicarle mas horas. Al pricipio parece imposible, pero notaras una mejora rapidamente.'
     },
+    {
+      tit: 'Programa proyectos propios',
+      desc: 'Mas vale 10 horas trabajando en proyectos propios, que 10 horas mirando tutoriales. La motivación y la implicación en el proyecto ayudara a acelerar tu aprendizaje.'
+    },
+    {
+      tit: 'Procura descansar',
+      desc: 'Descansar bien y desconectar son vitales. De esta manera reduciras el estres y la ansiedad. Mejoraras tu concentracion y consolidaras tu aprendizaje.'
+    }
   ];
+
+  const sigPaso = () => {
+    setPrimerPaso(primerPaso + 1);
+  };
 
 return (
     <div className="container mt-5">
@@ -19,7 +31,8 @@ return (
         <div className="col-md-6">
           <Card 
             titulo={tutorialData[primerPaso].tit}
-            descripcion={tutorialData[primerPaso].desc} 
+            descripcion={tutorialData[primerPaso].desc}
+            siguientePaso={sigPaso} 
           />
         </div>
       </div>
--- onboarding-digital/src/componentes/card.jsx
+++ onboarding-digital/src/componentes/card.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../hoja-de-estilos/card.css';
 
-const Card = ({ titulo, descripcion }) => {
+const Card = ({ titulo, descripcion, siguientePaso }) => {
   return (
     <div className="card border-primary mb-3">
       <div className="card-header">{titulo}</div>
         <div className="card-body text-primary">
           <p className="card-text">{descripcion}</p>
+          <button className="btn btn-primary" onClick={siguientePaso}>Siguiente</button>
         </div>
     </div>
     );
