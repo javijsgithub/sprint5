7d213c4286bdac7f5a3b90d1ce83a97b5847d732
Javi Sanchez <144378200+javijsgithub@users.noreply.github.com>
2024-01-30 01:43:35 +0100

    exercici 3

--- onboarding-digital/src/App.js
+++ onboarding-digital/src/App.js
@@ -8,14 +8,17 @@ function App() {
 
   const tutorialData = [
     {
+      img: 'time_managment',
       tit: 'Dedica muchas horas',
       desc: 'Un minimo de 30 horas a la semana. Si no tienes suficiente, tendras que dedicarle mas horas. Al pricipio parece imposible, pero notaras una mejora rapidamente.'
     },
     {
+      img: 'programming',
       tit: 'Programa proyectos propios',
       desc: 'Mas vale 10 horas trabajando en proyectos propios, que 10 horas mirando tutoriales. La motivación y la implicación en el proyecto ayudara a acelerar tu aprendizaje.'
     },
     {
+      img: 'meditation',
       tit: 'Procura descansar',
       desc: 'Descansar bien y desconectar son vitales. De esta manera reduciras el estres y la ansiedad. Mejoraras tu concentracion y consolidaras tu aprendizaje.'
     }
@@ -29,7 +32,8 @@ return (
     <div className="container mt-5">
       <div className="row justify-content-center">
         <div className="col-md-6">
-          <Card 
+          <Card
+            imagen={tutorialData[primerPaso].img} 
             titulo={tutorialData[primerPaso].tit}
             descripcion={tutorialData[primerPaso].desc}
             siguientePaso={sigPaso} 
--- onboarding-digital/src/componentes/card.jsx
+++ onboarding-digital/src/componentes/card.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import '../hoja-de-estilos/card.css';
+import { BsArrowRightCircleFill } from "react-icons/bs";
+import 'google-fonts';
 
-const Card = ({ titulo, descripcion, siguientePaso }) => {
+const Card = ({ titulo, descripcion, siguientePaso, imagen }) => {
   return (
-    <div className="card border-primary mb-3">
+    <div className="card mb-1">
+      <img src={require(`../imagenes/img-${imagen}.svg`)} className="card-img-top" alt="imagen" />
       <div className="card-header">{titulo}</div>
-        <div className="card-body text-primary">
+        <div className="card-body text-black">
           <p className="card-text">{descripcion}</p>
-          <button className="btn btn-primary" onClick={siguientePaso}>Siguiente</button>
+          <div className="avanzar-retroceder" onClick={siguientePaso}><BsArrowRightCircleFill className='boton-avanzar' /></div>
         </div>
     </div>
     );
   };
+
+  
 
   export default Card;
